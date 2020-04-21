d7f6f58a6e0fef94a03537718189decb837b4c48
Michael <michaeltesto15@hotmail.com>
2020-04-20 23:11:23 -0700

    Cell Spread V1

--- 2d art/src/com/company/Main.java
+++ 2d art/src/com/company/Main.java
@@ -7,31 +7,34 @@ import javax.imageio.ImageTranscoder;
 import javax.swing.*;
 
 public class Main extends JPanel {
-    public static int screenHeight = 750;
-    public static int screenWidth = 1000;
-    public static ArrayList<BouncyBall> balls = new ArrayList<>();
-
+    public static int screenHeight = 200;
+    public static int screenWidth = 500;
+    public static CellPanel cells;
 
     //Draws whatever you want.
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        for (BouncyBall ball: balls) {
-            g.setColor(ball.ballColor);
-            g.fillOval(ball.x, ball.y, ball.blockHeight, ball.blockHeight);
+        for (int i = 0; i < cells.cells.length; i++) {
+            for (int j = 0; j < cells.cells[i].length; j++) {
+                boolean cell = cells.cells[i][j];
+                if (cells.cells[i][j]){
+                    g.setColor(Color.BLACK);
+                    g.drawRect(j, i, 1, 1);
+                }else{
+                    g.setColor(Color.GRAY);
+                    g.drawRect(j, i, 1, 1);
+                }
+                cells.cells[i][j] = cell;
+            }
         }
-
-
     }
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
-            Random rand = new Random();
-            balls.add(new BouncyBall(rand.nextInt(screenWidth), 0));
-            balls.get(i).setBlockHeight((int) 25);
-            //balls.get(i).gravity = -1 *( new Random().nextInt(10)/2 + 1);
-            balls.get(i).gravity = -1 ;
+        cells = new CellPanel(screenWidth, 200);
+        cells.makeRandomActive();
+        cells.makeRandomActive();
+        cells.makeRandomActive();
 
-        }
 
         JFrame jf = new JFrame("Cell Spread");
         Main m = new Main();
@@ -41,9 +44,10 @@ public class Main extends JPanel {
         jf.add(m);
         m.repaint();
         Thread.sleep(1000);
-        for (BouncyBall ball: balls) {
-            ball.startBouncing(m, .55);
-            Thread.sleep((long) (Math.random() * 10));
+        while (true){
+            cells.age();
+            m.repaint();
+            Thread.sleep(20);
         }
 
     }
